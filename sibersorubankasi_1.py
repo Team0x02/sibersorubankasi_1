f049bdbe079cfc416bce3fa9d60b9e5de80198bf
Team0x02 <119530796+Team0x02@users.noreply.github.com>
2022-12-09 10:23:19 -0800

    Create sibersorubankasi_1.py

---
+++ sibersorubankasi_1.py
@@ -0,0 +1,192 @@
+print("""
+•
+•""")
+print("""SİBER SORU BANKASI hazırlayanlar;
+•
+•
+•""")
+print("\033[31m=======ghost0x02=======readlydex0x02====ratashi0x02=======)\033[0m")
+print("""
+•
+•
+•
+•""")
+print("""\033[31mPeki bu testte neler olacak?
+Bu testte 'LİNUX' tabanlı terminaller,'PYTHON' hakkında sorular,
+'SİBER GÜVENLİK' hakkında sorular,
+çeşitli 'YAZILIM' dilleri hakkında sorular olacaktır.
+Bu etkinlik 'DİSCORD' üzerinden yapılacaktır.)\033[0m""")
+print("""
+•
+•
+•""")
+print("2022-2023 SİBER SORU BANKASI👇")
+print("""
+•
+•
+•""")
+print("\033[31mSORU BANKASI 1.1v)\033[0m")
+
+print("\033[31mSORU 1)\033[0m")
+print("""Aşağıdakilerden hangisi bir yazılım dili değildir?
+
+A) Python
+
+B) C++
+
+C) c#
+
+D) Brute Force""")
+soru1=input("Cevabınız : ")
+print("""
+•
+•
+•""")
+print("\033[31mSORU 2)\033[0m")
+print("""Aşağıdakilerden hangisi 'AĞ' cihazlarından değildir?
+
+A) Telgraf
+
+B) Router
+
+C) Hub
+
+D) Switch""")
+soru2=input("Cevabınız : ")
+print("""
+•
+•
+•""")
+print("\033[31mSORU 3)\033[0m")
+print("""PYTHON için ekrana yazı yazdırılmasını sağlayan komut hangisidir?
+
+A) liste()
+
+B) string()
+
+C) len()
+
+D) print()""")
+
+soru3=input("Cevabınız : ")
+print("""
+•
+•
+•""")
+print("\033[31mSORU 4)\033[0m")
+print("""Aşağıdaki karakterlerden hangisi print komutunda birleştirme operatörü olarak kullanılır?
+
+A) Sorun işareti ?
+
+B) Virgül  ,
+
+C) Nokta  .
+
+D) Eşittir =""")
+soru4=input("Cevabınız : ")
+print("""
+•
+•
+•""")
+print("\033[31mSORU 5)\033[0m")
+print("""Python kodlama dilinde kullanıcıdan veri alınması gereken durumlarda
+ kullanılması gereken kod aşağıdakilerden hangisi?
+
+A) print()
+
+B) len()
+
+C) enter()
+
+D) input()""")
+soru5=input("Cevabınız : ")
+print("""
+•
+•
+•""")
+print("\033[31mSORU 6)\033[0m")
+print("""Metinsel bir ifadeyi sayısal bir ifadeye çevirmek için aşağıdaki komutlardan hangisi kullanılmaktadır?
+
+A) char()
+
+B) num()
+
+C) int()
+
+D) str()""")
+soru6=input("Cevabınız : ")
+print("""
+•
+•
+•""")
+print("\033[31mSORU 7)\033[0m")
+print("""Html etiketleri hangi karakterler arasına yazılır?
+
+A) < >
+B) { }
+C) “ “
+D) = =""")
+soru7=input("Cevabınız : ")
+print("""
+•
+•
+•""")
+print("\033[31mSORU 8)\033[0m")
+print("""HTML başlık etiketlerinde hangisinde yazı boyutu en büyüktür?
+
+A) <h4>
+
+B) <h1>
+
+C) <h2>
+
+D) <h3>""")
+soru8=input("Cevabınız : ")
+print("""
+•
+•
+•""")
+print("\033[31mSORU 9)\033[0m")
+print("""Bir web sayfası hangi HTML etiketi ile başlar?
+
+A) <title>
+
+B) <head>
+
+C) <html>
+
+D) <body>""")
+soru9=input("Cevabınız : ")
+print("""
+•
+•
+•""")
+print("\033[31mSORU 10)\033[0m")
+print("""PHPdeki yazım şekli nasıldır?
+
+A) <?php echo “Merhaba” ?>
+
+B) <php? echo “Merhaba”; ?>
+
+C) <?php echo Merhaba; ?>
+
+D) <?php echo “Merhaba”; ?>""")
+soru10=input("Cevabınız : ")
+
+
+
+
+print("""Cevaplar
+  1:D
+  2:A
+  3:D
+  4:B
+  5:D
+  6:C
+  7:A
+  8:A
+  9:B
+ 10:D
+""")
+print("\033[33mİLETİŞİM İÇİN İNSTAGRAM @ghost0x02 @readlydex0x02 @ratashi0x02\033[0m")
+print("\033[33mTEST BİTMİŞTİR\033[0m")
